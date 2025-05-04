ddb99898b16edb36d817df94208c0254f9fba90f
kuthntm <kuthntm@gmail.com>
2025-05-04 11:05:16 +0900

    lombok refactoring and Slf4j logging

--- src/main/java/com/example/SpringProject/controller/ArticleController.java
+++ src/main/java/com/example/SpringProject/controller/ArticleController.java
@@ -3,13 +3,18 @@ package com.example.SpringProject.controller;
 import com.example.SpringProject.dto.ArticleForm;
 import com.example.SpringProject.entity.Article;
 import com.example.SpringProject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j // 로깅 기능을 위한 어노테이션
 @Controller
 public class ArticleController {
+    private static final Logger log = LoggerFactory.getLogger(ArticleController.class);
     @Autowired // 스프링 부트가 미리 생성해 놓은 리파지터리 객체 주입
     private ArticleRepository articleRepository;
     @GetMapping("/articles/new")
@@ -19,13 +24,16 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){ // 폼데이터를 DTO 객체로 받기
-        System.out.println(form.toString());
+        log.info(form.toString());
+        //System.out.println(form.toString());
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
+        //System.out.println(article.toString());
         // 2. 리파지터리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+        //System.out.println(saved.toString());
         return "";
     }
 }
--- src/main/java/com/example/SpringProject/dto/ArticleForm.java
+++ src/main/java/com/example/SpringProject/dto/ArticleForm.java
@@ -1,23 +1,14 @@
 package com.example.SpringProject.dto;
 
 import com.example.SpringProject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor // 롬복을 이용한 리팩터링. 생성자 대체
+@ToString
 public class ArticleForm {
     private String title;
     private String content;
-
-    public ArticleForm(String title, String content) { // 생성자
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() { // 데이터를 잘 받았는지 확인할 메서드
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
