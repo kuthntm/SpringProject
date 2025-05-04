327a7ec6158ab29d9cdf457c7a988d5e79acc38a
kuthntm <kuthntm@gmail.com>
2025-05-05 05:50:16 +0900

    list data read

--- src/main/java/com/example/SpringProject/controller/ArticleController.java
+++ src/main/java/com/example/SpringProject/controller/ArticleController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+
 @Slf4j // 로깅 기능을 위한 어노테이션
 @Controller
 public class ArticleController {
@@ -45,5 +47,15 @@ public class ArticleController {
         model.addAttribute("article", articleEntity);
         // 3. 뷰 페이지 반환하기
         return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model){
+        // 1. 모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+        // 3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
