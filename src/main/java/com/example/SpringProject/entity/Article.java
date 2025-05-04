f72a999cd675635638b96c2584b864568481eb7b
kuthntm <kuthntm@gmail.com>
2025-05-05 08:36:44 +0900

    REST API Controller, CRUD

--- src/main/java/com/example/SpringProject/entity/Article.java
+++ src/main/java/com/example/SpringProject/entity/Article.java
@@ -1,27 +1,33 @@
 package com.example.SpringProject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 @Entity
+@ToString
+@Getter
 public class Article {
     @Id // 엔티티 대푯값
-    @GeneratedValue // 자동적으로 1씩 증가
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동적으로 1씩 증가
     private Long id;
     @Column
     private String title;
     @Column
     private String content;
 
-    public Long getId() { // Getter 메서드
-        return id;
+    public void patch(Article article) {
+        if (article.title != null)
+            this.title = article.title;
+        if (article.content != null)
+            this.content = article.content;
     }
+
+    /*public Long getId() { // Getter 메서드
+        return id;
+    }*/
 }
