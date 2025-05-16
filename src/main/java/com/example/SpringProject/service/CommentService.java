b7fcd72834fb1c319e44bc6c1953d353add1cec1
kuthntm <kuthntm@gmail.com>
2025-05-16 20:16:32 +0900

    comment controller, dto, service. read

---
+++ src/main/java/com/example/SpringProject/service/CommentService.java
@@ -0,0 +1,33 @@
+package com.example.SpringProject.service;
+
+import com.example.SpringProject.dto.CommentDto;
+import com.example.SpringProject.entity.Comment;
+import com.example.SpringProject.repository.ArticleRepository;
+import com.example.SpringProject.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId) {
+        // 1. 댓글 조회
+        List<Comment> comments = commentRepository.findByArticleId(articleId);
+        // 2. 엔티티 -> DTO 변환
+        List<CommentDto> dtos = new ArrayList<CommentDto>();
+        for ( int i = 0; i < comments.size(); i++){
+            Comment c = comments.get(i);
+            CommentDto dto = CommentDto.createCommentDto(c);
+            dtos.add(dto);
+        }
+        // 3. 결과 반환
+        return dtos;
+    }
+}
