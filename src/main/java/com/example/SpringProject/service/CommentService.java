bc16341d5ab171d829852ef81a7f134c39ff082e
kuthntm <kuthntm@gmail.com>
2025-05-21 23:01:33 +0900

    comment create

--- src/main/java/com/example/SpringProject/service/CommentService.java
+++ src/main/java/com/example/SpringProject/service/CommentService.java
@@ -1,11 +1,13 @@
 package com.example.SpringProject.service;
 
 import com.example.SpringProject.dto.CommentDto;
+import com.example.SpringProject.entity.Article;
 import com.example.SpringProject.entity.Comment;
 import com.example.SpringProject.repository.ArticleRepository;
 import com.example.SpringProject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,5 +31,17 @@ public class CommentService {
         }
         // 3. 결과 반환
         return dtos;
+    }
+
+    @Transactional
+    public CommentDto create(Long articleId, CommentDto dto) {
+        // 1. 게시글 조회 및 예외 발생
+        Article article = articleRepository.findById(articleId).orElseThrow(() -> new IllegalArgumentException("댓글 생성 실패" + "대상 게시글이 없습니다."));
+        // 2. 댓글 엔티티 생성
+        Comment comment = Comment.createComment(dto, article);
+        // 3. 댓글 엔티티를 DB에 저장
+        Comment created = commentRepository.save(comment);
+        // 4. DTO로 변환해 반환
+        return CommentDto.createCommentDto(created);
     }
 }
