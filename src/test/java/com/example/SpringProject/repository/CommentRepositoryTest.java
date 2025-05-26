2d4b249a9fac26ace7cf862af237ee3ea1b8a074
kuthntm <kuthntm@gmail.com>
2025-05-27 00:40:15 +0900

    test application properties update3

--- src/test/java/com/example/SpringProject/repository/CommentRepositoryTest.java
+++ src/test/java/com/example/SpringProject/repository/CommentRepositoryTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+@ActiveProfiles("test")
 @DataJpaTest
 class CommentRepositoryTest {
     @Autowired
