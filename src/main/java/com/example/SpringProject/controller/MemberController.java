627bb847d1028fd856e6ed8b2a5e6935ca846bbd
kuthntm <kuthntm@gmail.com>
2025-05-30 17:17:21 +0900

    member read

--- src/main/java/com/example/SpringProject/controller/MemberController.java
+++ src/main/java/com/example/SpringProject/controller/MemberController.java
@@ -5,8 +5,12 @@ import com.example.SpringProject.entity.Member;
 import com.example.SpringProject.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -23,5 +27,19 @@ public class MemberController {
         Member member = form.toEntity();
         Member saved = memberRepository.save(member);
         return "redirect:/members/" + saved.getId();
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", memberEntity);
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model){
+        List<Member> memberEntityList = (List<Member>) memberRepository.findAll();
+        model.addAttribute("memberList", memberEntityList);
+        return "members/index";
     }
 }
