21f517a74ae83b35d2db5f4df7c041e0457fdf58
kuthntm <kuthntm@gmail.com>
2025-05-30 15:39:05 +0900

    Test fix

--- src/test/java/com/example/SpringProject/service/ArticleServiceTest.java
+++ src/test/java/com/example/SpringProject/service/ArticleServiceTest.java
@@ -2,9 +2,12 @@ package com.example.SpringProject.service;
 
 import com.example.SpringProject.dto.ArticleForm;
 import com.example.SpringProject.entity.Article;
+import com.example.SpringProject.repository.ArticleRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,16 +21,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class ArticleServiceTest {
     @Autowired
     ArticleService articleService;
+
     @Test
     void index() {
-        // 1. 예상 데이터
-        Article a = new Article(1L, "가가가가", "1111");
-        Article b = new Article(2L, "나나나나", "2222");
-        Article c = new Article(3L, "다다다다", "3333");
-        List<Article> expected = new ArrayList<Article>(Arrays.asList(a, b, c));
-        // 2. 실제 데이터
+        List<Article> expected = new ArrayList<>(Arrays.asList(
+                new Article(1L, "가가가가", "1111"),
+                new Article(2L, "나나나나", "2222"),
+                new Article(3L, "다다다다", "3333"),
+                new Article(4L, "영화 추천", "댓글 부탁"),
+                new Article(5L, "음식 추천", "댓글 달아주세요"),
+                new Article(6L, "취미 추천", "부탁드립니다")
+        ));
         List<Article> articles = articleService.index();
-        // 3. 비교 및 검증
         assertEquals(expected.toString(), articles.toString());
     }
 
@@ -54,7 +59,7 @@ class ArticleServiceTest {
         String title = "라라라라";
         String content = "4444";
         ArticleForm dto = new ArticleForm(null, title, content);
-        Article expected = new Article(4L, title, content);
+        Article expected = new Article(7L, title, content);
 
         Article article = articleService.create(dto);
 
